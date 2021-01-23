849b4b9717ff28b620f31442fd9f69c0e6375405
tesselock <tesselock@users.noreply.github.com>
2021-01-22 18:44:09 -0600

    changes

--- colorstrand/src/App.js
+++ colorstrand/src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
-
+//this is a component that holds all the other componentsd
 function App() {
+  const varss = "trisaaa";
+  const styles = {
+    backgroundColor: "blue"
+    // if there is supposed to be a dash like background-color, change it to 
+    //camelcase
+  }
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 style = {styles}>hello {varss} is me</h1>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
       </header>
     </div>
   );
--- colorstrand/src/index.js
+++ colorstrand/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* creates an instance of the component by wrapping it in JSX */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
