0c2f2e0789b6b7dc3fe097d3f03eeabb305bea9b
tesselock <tesselock@users.noreply.github.com>
2021-02-09 15:19:47 -0600

    states

--- colorstrand/src/App.js
+++ colorstrand/src/App.js
@@ -2,32 +2,72 @@
 import './App.css';
 import Item from './Item';
 import fakeData from './fakeData';
+import { Component } from 'react';
 //this is a component that holds all the other componentsd
-function App() {
- const datacomp= fakeData.map(d => {
-   return (<Item name={d.name} email={d.email} />)
- })
+//if you want to use state, you need a class based component\
+// function App() {
+//  const datacomp= fakeData.map(d => {
+//    return (<Item name={d.name} email={d.email} checked={d.checked}/>)
+//  })
 
 
-  const varss = "trisaaa";
-  //use curly braces to use javascript variables in JSX
-  const styles = {
-    backgroundColor: "#d8dde6",
-    width : "30%",
-    margin: "auto"
-    // if there is supposed to be a dash like background-color, change it to 
-    //camelcase
+//   const varss = "trisaaa";
+//   //use curly braces to use javascript variables in JSX
+//   const styles = {
+//     backgroundColor: "#d8dde6",
+//     width : "30%",
+//     margin: "auto"
+//     // if there is supposed to be a dash like background-color, change it to 
+//     //camelcase
+//   }
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1 style = {styles}>hello {varss} is me</h1>
+//         <Item name="placeholder" lastname="kek"/> 
+//         {datacomp}
+//         {/* name is the first prop */}
+//       </header>
+//     </div>
+//   );
+// }
+
+
+class App extends Component{
+  constructor(){
+    //initializes part of the class
+    super()
+    this.state = {
+      answer: "yes"
+    }
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 style = {styles}>hello {varss} is me</h1>
-        <Item name="placeholder" lastname="kek"/> 
-        {datacomp}
-        {/* name is the first prop */}
-      </header>
-    </div>
-  );
+  render()
+  {
+    const datacomp= fakeData.map(d => {
+      return (<Item name={d.name} email={d.email} checked={d.checked}/>)
+    })
+   
+   
+     const varss = "trisaaa";
+     //use curly braces to use javascript variables in JSX
+     const styles = {
+       backgroundColor: "#d8dde6",
+       width : "30%",
+       margin: "auto"
+       // if there is supposed to be a dash like background-color, change it to 
+       //camelcase
+     }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 style = {styles}>is {varss} going to learn react? hopefully {this.state.answer}</h1>
+          <Item name="placeholder" lastname="kek"/> 
+          {datacomp}
+          {/* name is the first prop */}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- colorstrand/src/Item.js
+++ colorstrand/src/Item.js
@@ -1,15 +1,30 @@
-
-function Item(props){
+import React from "react"
+// function Item(props){
     
-    return(
-        <div>
-        <input type= "checkbox"/>
-        <p style = {{color: props.email ? "red" : "black"}}> Name: {props.name}</p>
-        <p style = {{display: props.email ? "block" : "none"}}> Email: {props.email}</p>
+//     return(
+//         <div>
+//         <input type= "checkbox" checked = {props.checked}/>
+//         <p style = {{color: props.email ? "red" : "black"}}> Name: {props.name}</p>
+//         <p style = {{display: props.email ? "block" : "none"}}> Email: {props.email}</p>
         
-        {/* if it doesnt exist, have it not display */}
-        </div>
-        //props: pass in data within html ?? 
-    )
+//         {/* if it doesnt exist, have it not display */}
+//         </div>
+//         //props: pass in data within html ?? 
+//     )
+// }
+
+class Item extends React.Component{
+    render() {
+        return(
+            <div>
+            <input type= "checkbox" checked = {this.props.checked}/>
+            <p style = {{color: this.props.email ? "red" : "black"}}> Name: {this.props.name}</p>
+            <p style = {{display: this.props.email ? "block" : "none"}}> Email: {this.props.email}</p>
+            
+            {/* if it doesnt exist, have it not display */}
+            </div>
+            //props: pass in data within html ?? 
+        )
+    }
 }
 export default Item
